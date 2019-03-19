c54cf11b35780098b89423f0fafd3875071c60d6
Claudio Sanches <contato@claudiosanches.com>
2019-03-19 12:00:27 -0300

    Options to skip GH or SVN, also refactored code into functions

--- release.sh
+++ release.sh
@@ -20,6 +20,27 @@ if [ -z $GITHUB_ACCESS_TOKEN ]; then
   exit
 fi
 
+# Define options
+SKIP_GH=false
+SKIP_SVN=false
+
+# Set user options
+while [ ! $# -eq 0 ]; do
+  case "$1" in
+    -h|--help)
+      echo "Help Message"
+      exit;
+      ;;
+    -g|--skip-gh)
+      SKIP_GH=true
+      ;;
+    -s|--skip-svn)
+      SKIP_SVN=true
+      ;;
+  esac
+  shift
+done
+
 # Ask info
 read -p "VERSION: " VERSION
 read -p "BRANCH: " BRANCH
@@ -27,9 +48,10 @@ echo "-------------------------------------------"
 read -r -p "You are about to release \"${VERSION}\" based on \"${BRANCH}\" GIT branch. Are you sure? [y/N]" RESPONSE
 case "$RESPONSE" in
   [yY])
-    echo "Accepted and moving on..."
+    echo "Confirmed! Moving on..."
     ;;
   *)
+    echo "Release cancelled!"
     exit;
     ;;
 esac
@@ -84,75 +106,97 @@ copy_dest_files() {
   cd $SVN_PATH
 }
 
-# Check if is a pre-release.
-if is_substring "-" ${VERSION}; then
-  IS_PRE_RELEASE="true"
+# Run JS build.
+run_js_build() {
+  echo "Running JS Build..."
+  cd $GIT_PATH
+  npm install
+  npm run build || exit "$?"
+}
+
+# Create SVN release
+create_svn_release() {
+  # Create build directory if does not exists
+  if [ ! -d $BUILD_PATH ]; then
+    mkdir -p $BUILD_PATH
+  fi
+
+  # Checkout SVN repository if not exists
+  if [ ! -d $SVN_PATH ]; then
+    echo "No SVN directory found, will do a checkout..."
+    svn checkout $SVN_REPO $SVN_PATH
+  fi
+
+  # Delete old GIT directory
+  rm -Rf $GIT_PATH
+
+  # Clone GIT repository
+  echo "Cloning GIT repository..."
+  git clone $GIT_REPO $GIT_PATH --branch ${BRANCH} --single-branch || exit "$?"
+
+  # Run grunt
+  run_js_build
+
+  # Move into SVN directory
+  cd $SVN_PATH
+
+  # Update SVN
+  echo "Updating SVN..."
+  svn update
+
+  # Copy GIT directory to trunk
+  copy_dest_files "trunk"
+
+  # Do the remove all deleted files
+  svn rm $( svn status | sed -e '/^!/!d' -e 's/^!//' )
+
+  # Do the add all not know files
+  svn add --force * --auto-props --parents --depth infinity -q
+
+  # Copy trunk to tag/$VERSION
+  if [ ! -d "tags/${VERSION}" ]; then
+    svn copy trunk tags/${VERSION}
+  else
+    # Just copy again the files if tag/$VERSION already exists
+    # This prevents creation of tag/$VERSION/trunk directory
+    copy_dest_files "tags/${VERSION}"
+  fi
+
+  # Remove the GIT directory
+  echo "Removing GIT directory..."
+  rm -Rf $GIT_PATH
+}
+
+# Create GH release
+create_gh_release() {
+  echo "Creating GITHUB release..."
+
+  # Check if is a pre-release.
+  if is_substring "-" ${VERSION}; then
+    IS_PRE_RELEASE="true"
+  fi
+
+  API_JSON=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "%s","body": "Release of version %s","draft": false,"prerelease": %s}' $VERSION $BRANCH $VERSION $VERSION $IS_PRE_RELEASE)
+
+  curl --data "$API_JSON" https://api.github.com/repos/woocommerce/${PRODUCT_NAME}/releases?access_token=${GITHUB_ACCESS_TOKEN}
+}
+
+if ! $SKIP_SVN; then
+  create_svn_release
 fi
-
-# Create build directory if does not exists
-if [ ! -d $BUILD_PATH ]; then
-  mkdir -p $BUILD_PATH
-fi
-
-# Checkout SVN repository if not exists
-if [ ! -d $SVN_PATH ]; then
-  echo "No SVN directory found, will do a checkout..."
-  svn checkout $SVN_REPO $SVN_PATH
-fi
-
-# Delete old GIT directory
-rm -Rf $GIT_PATH
-
-# Clone GIT repository
-echo "Cloning GIT repository..."
-git clone $GIT_REPO $GIT_PATH --branch ${BRANCH} --single-branch || exit "$?"
-
-# Run grunt
-echo "Running JS Build..."
-cd $GIT_PATH
-npm install
-npm run build || exit "$?"
-
-# Move into SVN directory
-cd $SVN_PATH
-
-# Update SVN
-echo "Updating SVN..."
-svn update
-
-# Copy GIT directory to trunk
-copy_dest_files "trunk"
-
-# Do the remove all deleted files
-svn rm $( svn status | sed -e '/^!/!d' -e 's/^!//' )
-
-# Do the add all not know files
-svn add --force * --auto-props --parents --depth infinity -q
-
-# Copy trunk to tag/$VERSION
-if [ ! -d "tags/${VERSION}" ]; then
-  svn copy trunk tags/${VERSION}
-else
-  # Just copy again the files if tag/$VERSION already exists
-  # This prevents creation of tag/$VERSION/trunk directory
-  copy_dest_files "tags/${VERSION}"
-fi
-
-# Remove the GIT directory
-echo "Removing GIT directory..."
-rm -Rf $GIT_PATH
 
 # Create the GitHub release
-echo "Creating GITHUB release..."
-API_JSON=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "%s","body": "Release of version %s","draft": false,"prerelease": %s}' $VERSION $BRANCH $VERSION $VERSION $IS_PRE_RELEASE)
-curl --data "$API_JSON" https://api.github.com/repos/woocommerce/${PRODUCT_NAME}/releases?access_token=${GITHUB_ACCESS_TOKEN}
+if ! $SKIP_GH; then
+  create_gh_release
+fi
 
-# Do svn commit
-svn status
-echo "Ready to commit into WordPress.org Plugin's Directory!"
-echo "Run the follow commads to commit:"
-echo "cd ${SVN_PATH}"
-echo "svn commit -m \"Release "${VERSION}", see readme.txt for changelog.\""
+if ! $SKIP_SVN; then
+  # SVN commit messsage
+  echo "Ready to commit into WordPress.org Plugin's Directory!"
+  echo "Run the follow commads to commit:"
+  echo "cd ${SVN_PATH}"
+  echo "svn commit -m \"Release "${VERSION}", see readme.txt for changelog.\""
+fi
 
 # Done
-echo "WooCommerce releaser done"
+echo "Release complete!"
